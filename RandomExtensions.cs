63b6aa723ef12285c2d36537875acf24b046eb5c
xivaer <xivaer@hotmail.com>
2020-05-15 09:41:25 +0800

    Add files via upload

---
+++ RandomExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Source: http://stackoverflow.com/a/110570
+static class RandomExtensions
+{
+	//洗牌判斷
+	public static void Shuffle<T> (this Random rng, T[] array)
+	{
+		int n = array.Length;
+		while (n > 1) 
+		{
+			int k = rng.Next(n--);
+			T temp = array[n];
+			array[n] = array[k];
+			array[k] = temp;
+		}
+	}
+}
